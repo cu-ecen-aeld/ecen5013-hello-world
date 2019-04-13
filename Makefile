15d9447cfec8a57a07f029d97b1578ab6921fdb6
Dan Walkes <danwalkes@trellis-logic.com>
2019-04-13 11:33:36 -0600

    Initial commit
    Demonstrate a simple hello world Makefile project.

---
+++ Makefile
@@ -0,0 +1,5 @@
+main.o: main.c
+	$(CC) main.c -o hello-ecen5013
+
+clean :
+	rm main.o hello-ecen5013
---
+++ main.c
@@ -0,0 +1,10 @@
+/**
+* Simple example for C project build
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello and welcome to ECEN5013!\n");
+}
